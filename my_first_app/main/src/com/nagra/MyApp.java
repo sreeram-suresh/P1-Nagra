85e6f6fb855fa8c822dc1e26714726a3479b52e1
Sreeram Suresh <sreeramsuresh27@gmail.com>
2023-04-20 14:03:28 +0530

    Functionalities added to generate a ASCII code from an image

--- my_first_app/main/src/com/nagra/MyApp.java
+++ my_first_app/main/src/com/nagra/MyApp.java
@@ -1,7 +1,44 @@
 package com.nagra;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+
 
 public class MyApp {
     public static void main(String[] args) {
+
         System.out.println("Hello!!, It's Sreeram here");
+        try {
+            // Load the image file
+            File imageFile = new File("src/main/resources/Images/brain-mech.jpg");
+            BufferedImage image = ImageIO.read(imageFile);
+
+            // Resize the image to a smaller size for better ASCII representation
+            int newWidth = image.getWidth() / 8;
+            int newHeight = image.getHeight() / 16;
+            BufferedImage resizedImage = new BufferedImage(newWidth, newHeight, BufferedImage.TYPE_INT_RGB);
+            resizedImage.createGraphics().drawImage(image, 0, 0, newWidth, newHeight, null);
+
+            // Convert the resized image to ASCII characters
+            StringBuilder sb = new StringBuilder();
+            String characters = "@#S%?+;:,.";// ASCII characters from darkest to lightest
+            for (int y = 0; y < newHeight; y++) {
+                for (int x = 0; x < newWidth; x++) {
+                    int rgb = resizedImage.getRGB(x, y);
+                    int gray = (int) (0.2126 * ((rgb >> 16) & 0xff) + 0.7152 * ((rgb >> 8) & 0xff) + 0.0722 * (rgb & 0xff));
+                    int index = (int) (gray / (255.0 / (characters.length() - 1)));
+                    sb.append(characters.charAt(index));
+                }
+                sb.append("\n");
+            }
+
+            // Print the ASCII art
+            System.out.println(sb.toString());
+
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
     }
+
 }
